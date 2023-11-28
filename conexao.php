788232aae888838dbdbfe760dd04093ccd46761d
nakanoyur1 <124378059+nakanoyur1@users.noreply.github.com>
2023-11-28 11:41:53 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -4,10 +4,9 @@ $username = "root";
 $password = "";
 $dbname = "PROVA";
 
-// Criar conexão
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar conexão
 if ($conn->connect_error) {
   die("Falha na conexão: " . $conn->connect_error);
 }
