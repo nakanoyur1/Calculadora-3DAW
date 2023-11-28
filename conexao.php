08bf5b8a3bb9a8ccf7cb70e10ae85b1c22c3b071
nakanoyur1 <124378059+nakanoyur1@users.noreply.github.com>
2023-11-28 11:08:17 -0300

    ADICIONEI OS ARQUIVOS DA AV2
    algo nessa maquina não permite fazer commit do meu arquivo, parece ser algum erro no SSH, entao peguei um outro repositorio que tinha aqui e adicionei os arquivos da AV2

---
+++ conexao.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "PROVA";
+
+// Criar conexão
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexão
+if ($conn->connect_error) {
+  die("Falha na conexão: " . $conn->connect_error);
+}
+?>
